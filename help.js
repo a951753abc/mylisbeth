007feb428ceaad407415faf2ef5210b347282117
Shion <a951753abc@gmail.com>
2020-06-04 18:58:30 +0800

    Modify help增加info指令

--- help.js
+++ help.js
@@ -7,7 +7,7 @@ const exampleEmbed = new Discord.MessageEmbed()
         { name: 'help', value: '呼叫這個選單' },
         { name: 'create [人物名稱]', value: '建立你的人物' },
         { name: 'move [武器名稱]', value: '鍛造你的武器給人去冒險' },
-        //{ name: 'info', value: '觀看人物資訊' },
+        { name: 'info', value: '觀看人物資訊' },
     )
     .setTimestamp();
 module.exports = function (cmd, userId) {
