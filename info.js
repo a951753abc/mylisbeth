39a9a0b13e3100af1b9f53ac4e3d82a54779be01
Shion <a951753abc@gmail.com>
2020-06-06 13:13:45 +0800

    Modify 顯示挖礦等級經驗與素材存量

--- info.js
+++ info.js
@@ -1,16 +1,29 @@
 const _ = require('lodash');
 const Discord = require('discord.js');
 const db = require("./db.js");
+const type = require("./type.js");
 module.exports = async function (cmd, userId) {
     let query = {userId: userId}
     let user = await db.findOne("user", query);
     let lose = _.get(user, "lost", 0);
     let cName = _.get(user, "name", "");
-    return new Discord.MessageEmbed()
+    let mes = new Discord.MessageEmbed()
         .setColor('#0099ff')
-        .setTitle("鍛造師" + cName)
-        .addFields(
-            {name: '拿著你鍛造武器冒險死亡人數', value: lose},
-        )
-        .setTimestamp();
+        .setTitle("鍛造師" + cName);
+    let itemListText = "";
+    if (_.get(user, "itemStock", 0) === 0) {
+        itemListText = "無";
+    } else {
+        _.forEach(user.itemStock, function (value) {
+            itemListText += "[" + type.ssrList(value.itemLevel) + "]" + value.itemName + " 數量:" + value.itemNum + "\n";
+        });
+    }
+    mes.addFields(
+        {name: '拿著你鍛造武器冒險死亡人數', value: lose},
+        {name: '挖礦等級', value: _.get(user, "mineLevel", 1), inline:true},
+        {name: '挖礦經驗值', value: _.get(user, "mine", 1), inline:true},
+        {name: '素材', value: itemListText},
+    ).setTimestamp();
+
+    return mes;
 }
--- type.js
+++ type.js
@@ -1,4 +1,8 @@
 const typeList = {mine:"挖礦"};
+const ssrList = {3:"SSR", 2:"SR", 1:"R"};
 module.exports = function (type) {
     return typeList[type];
+}
+module.exports.ssrList = function (rare) {
+    return ssrList[rare];
 }
